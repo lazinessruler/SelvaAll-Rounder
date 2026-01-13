55b8fd5c02b0e2c477bf1d89f9ec1e7920d50e09
𝖸𝖺𝗁𝗂𝗄𝗈 !! <lazinessruler@gmail.com>
2026-01-13 13:53:57 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+Main.py pe
+
+import config
+import time
+import logging
+from pyrogram import Client, idle
+from pyromod import listen
+from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
+
+Configure logging
+
+logging.basicConfig(
+level=logging.INFO,
+format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+logging.getLogger("pymongo").setLevel(logging.ERROR)
+
+Initialize start time
+
+StartTime = time.time()
+
+Initialize the Client
+
+app = Client(
+"Anonymous",
+api_id=config.API_ID,
+api_hash=config.API_HASH,
+bot_token=config.BOT_TOKEN,
+in_memory=True,
+plugins=dict(root="Flexyy"),
+)
+
+if name == "main":
+print("𝐹𝑙𝑒𝑥𝑦𝑦 𝑆𝑡𝑟𝑖𝑛𝑔 𝑆𝑒𝑠𝑠𝑖𝑜𝑛 𝐺𝑒𝑛.. 𝑠𝑡𝑎𝑟𝑡𝑖𝑛𝑔...")
+try:
+app.start()
+except ApiIdInvalid:
+raise Exception("Your API_ID is not valid.")
+except ApiIdPublishedFlood:
+raise Exception("Your API_ID/API_HASH is flood banned.")
+except AccessTokenInvalid:
+raise Exception("Your BOT_TOKEN is not valid.")
+except Exception as e:
+logging.error(f"An unexpected error occurred: {e}")
+raise
+
+uname = app.get_me().username  
+print(f"@{uname} NOW FLEXYY SESSION GEN IS READY TO GEN SESSION")  
+  
+idle()  
+  
+app.stop()  
+print("𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐢𝐧𝐠 𝐒𝐭𝐨𝐩𝐩...")
+
+Ye add kardu to koi problem to nahi ?
